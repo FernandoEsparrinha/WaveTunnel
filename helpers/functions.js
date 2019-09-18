6077dc168ac025c6b3f19cc1297f3a7ca52e509f
Fernando Esparrinha Pinto <fernando.esparrinha@gmail.com>
2019-09-18 22:49:17 +0100

    Implements isMobileDevice function

--- helpers/functions.js
+++ helpers/functions.js
@@ -4,3 +4,7 @@ function drawLine(x1, y1, x2, y2){
     vertex(x2,y2);  
     endShape();
 }
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
