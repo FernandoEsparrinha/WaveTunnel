b54611170e3d35364bf3f38d259934dc1c28fd6b
FernandoPinto <fernando.esparrinha@gmail.com>
2019-11-17 11:21:29 +0000

    Add new color mode into settings

--- helpers/variables.js
+++ helpers/variables.js
@@ -2,6 +2,7 @@
 
 let settings = {
     colorMode: ['two-tone', 'white', 'random'],
+    colorMode: ['two-tone', 'white', 'random', 'black-white'],
     backgroundColor: [0, 0, 0],
     debugMode: location.hostname === "",
     rotate: false,
