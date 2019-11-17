3cc23680d43fbb77c045c3d9c73d4c50f6b18d40
FernandoPinto <fernando.esparrinha@gmail.com>
2019-11-17 11:20:36 +0000

    Improve wave interaction + form color management

--- models/Rectangle.js
+++ models/Rectangle.js
@@ -11,6 +11,13 @@ class Rectangle {
 
         this.speed = initialSpeed;
         this.lifespan = 0;
+
+        this.grayTone = random(255);
+
+        this.x = translateX;
+        this.y = translateY;
+
+        this.rotationSpeed = (rotationSpeed / 200);
     }
 
     refreshSpeed(speed) {
@@ -18,45 +25,40 @@ class Rectangle {
     }
 
     reset() {
-        this.height = this.initialHeight;
-        this.width = this.initialWidth;
         this.iteration = 0;
     }
 
     move() {
         this.lifespan++;
-        if (this.height < -windowHeight && this.width < -windowWidth) {
+        if (this.iteration > 80 || this.iteration < -80) {
             this.reset();
-        } else if (this.height < windowHeight && this.width < windowWidth) {
-            this.iteration += this.speed;
-            this.width += this.iteration * (windowWidth / windowHeight);
-            this.height += this.iteration;
         } else {
-            //waveAudioSample.play();
-            this.reset();
+            this.iteration += this.speed;
         }
     }
 
     paintStroke() {
         let rectColor = [
-            Math.abs(this.width) / 2,
-            this.height / 2,
-            Math.abs(this.iteration) / 2
+            Math.abs(this.iteration) * 10,
+            0,
+            0
         ];
         stroke(rectColor)
     }
 
     display() {
         switch (settings.colorMode) {
+            case "two-tone":
+                this.paintStroke();
+                break;
             case "white":
                 stroke(color(255, 255, 255));
                 break;
             case "random":
-                stroke(color(random(this.width), random(this.width), random(this.width)));
+                stroke(color(random(255), random(255), random(255)));
                 break;
-            case "two-tone":
-                this.paintStroke();
-                break;
+            case "black-white":
+                stroke(color(this.grayTone, this.grayTone, this.grayTone));
             default:
                 break;
         }
@@ -66,17 +68,47 @@ class Rectangle {
         noFill();
 
         push();
-
         translate(windowWidth / 2, windowHeight / 2);
         scale(this.iteration);
 
-        strokeWeight(0.1);
         if (settings.rotate) {
-            rotate(this.lifespan / (rotationSpeed / 20));
+            // rotate(this.lifespan * this.rotationSpeed);
+            rotate(this.lifespan * (rotationSpeed / 200));
         }
+        strokeWeight(strokeWeigth);
         rect(0, 0, windowWidth / 20, windowHeight / 20);
-        // circle(0, 0, 50);
-
         pop();
     }
+}
+
+function drawPolygon(n) {
+    ang = ((180 * (n - 2)) / n) * (Math.PI / 180);
+    alt = Math.tan(ang / 2) * 10;
+
+    translate(-10, alt);
+
+    v0 = [0, 0];
+    v1 = [20, 0];
+
+    beginShape();
+    vertex(v0[0], v0[1]);
+    vertex(v1[0], v1[1]);
+    for (i = 0; i < n - 2; i++) {
+        x = v1[0] + (v0[0] - v1[0]) * Math.cos(ang) - (v0[1] - v1[1]) * Math.sin(ang);
+        y = v1[1] + (v0[0] - v1[0]) * Math.sin(ang) + (v0[1] - v1[1]) * Math.cos(ang);
+
+        vertex(x, y);
+        v0 = v1;
+        v1 = [x, y];
+    }
+    endShape(CLOSE);
+}
+
+function drawHalfTriangle() {
+    translate()
+    beginShape();
+    line(0, 0, -20, 20);
+    line(0, 0, 20, 20);
+    line(-20, 20, 20, 20);
+    endShape(CLOSE);
 }
