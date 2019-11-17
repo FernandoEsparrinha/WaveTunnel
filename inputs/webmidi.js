034266a4c90d61133302fb0013b65505d5e4b289
FernandoPinto <fernando.esparrinha@gmail.com>
2019-11-17 11:21:06 +0000

    Improve midi controller interaction

--- inputs/webmidi.js
+++ inputs/webmidi.js
@@ -5,28 +5,46 @@ WebMidi.enable(function (err) {
         console.log("WebMidi enabled!");
     }
 
-    console.log(WebMidi.inputs);
-    console.log(WebMidi.outputs);
+    // console.log(WebMidi.inputs);
+    // console.log(WebMidi.outputs);
     var input = WebMidi.inputs[0];
+
+    input.addListener('programchange', "all",
+        function (e) {
+            // TO DO - IMPLEMENT NEW FORMS DEPENDING ON THE PROGRAM 
+            console.log(e.data[1])
+        }
+    );
 
     input.addListener('controlchange', "all",
         function (e) {
-            if (e.data[1] == 1) {
-                settings.backgroundColor[0] = e.data[2]
-            }
-            if (e.data[1] == 2) {
-                settings.backgroundColor[1] = e.data[2]
-            }
-            if (e.data[1] == 3) {
-                settings.backgroundColor[2] = e.data[2]
+
+            if (e.data[1] == 1 || e.data[1] == 2 || e.data[1] == 3) {
+                let percentage = (100 * e.data[2]) / 127
+                settings.backgroundColor[e.data[1] - 1] = 255 * percentage / 100
             }
 
             if (e.data[1] == 4) {
-                rectangleWave.speed = (e.data[2] - 63) / 100;
+                rectangleWave.speed = map(e.data[2], 0, 127, -1, 1);
             }
+
             if (e.data[1] == 8) {
-                rotationSpeed = (e.data[2] - 63) / 10;
+                rotationSpeed = map(e.data[2], 0, 127, -2, 2);
             }
+
+            if (e.data[1] == 7) {
+                strokeWeigth = map(e.data[2], 0, 127, 0.01, 1);
+            }
+
+            if (e.data[1] == 5) {
+                translateX = map(e.data[2], 0, 127, 0, windowWidth);
+            }
+
+            if (e.data[1] == 6) {
+                translateY = map(e.data[2], 0, 127, 0, windowHeight);
+            }
+
+
 
             if (settings.debugMode) {
                 console.log("--------------------");
@@ -44,12 +62,24 @@ WebMidi.enable(function (err) {
                 console.log("Received 'noteon' message (" + e.note.name + " - " + e.note.octave + ").");
             }
             if (e.note.name == "G" && e.note.octave == "2") {
-                rectangleWave.increaseWaves();
+                rectangleWave.addWaves(1);
             }
             if (e.note.name == "D#" && e.note.octave == "2") {
-                rectangleWave.decreaseWaves();
+                rectangleWave.removeWaves(1);
+            }
+            if (e.note.name == "F#" && e.note.octave == "2") {
+                rectangleWave.addWaves(10);
             }
             if (e.note.name == "D" && e.note.octave == "2") {
+                rectangleWave.removeWaves(10);
+            }
+            if (e.note.name == "F" && e.note.octave == "2") {
+                rectangleWave.addWaves(100);
+            }
+            if (e.note.name == "C#" && e.note.octave == "2") {
+                rectangleWave.removeWaves(100);
+            }
+            if (e.note.name == "C" && e.note.octave == "2") {
                 settings.rotate = !settings.rotate;
             }
         }
