ad3f6a964164822518f181832503daec6a235251
FernandoPinto <fernando.esparrinha@gmail.com>
2019-11-17 11:51:24 +0000

    Add global variables that were missing

--- helpers/variables.js
+++ helpers/variables.js
@@ -1,10 +1,15 @@
 
 
 let settings = {
-    colorMode: ['two-tone', 'white', 'random'],
     colorMode: ['two-tone', 'white', 'random', 'black-white'],
     backgroundColor: [0, 0, 0],
     debugMode: location.hostname === "",
     rotate: false,
 };
 
+let rectangleWave;
+let gui;
+let rotationSpeed = 1;
+let strokeWeigth = 0.1;
+let translateX = 0;
+let translateY = 0;
