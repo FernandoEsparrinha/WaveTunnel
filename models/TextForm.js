3282df17e4addff2a0c680ff94bf4f051582b250
FernandoPinto <fernando.esparrinha@gmail.com>
2020-02-06 20:28:29 +0000

    Add Text form

---
+++ models/TextForm.js
@@ -0,0 +1,25 @@
+class TextForm extends Form {
+    constructor(initialSpeed) {
+        super(initialSpeed);
+        this.numberOfSides = globalSettings.wave.sides;
+    }
+
+    display() {
+        super.display();
+        rectMode(CENTER);
+        noFill();
+
+        push();
+        translate(width / 2, height / 2);
+
+        if (globalSettings.rotation.rotate) {
+            rotate(this.rotationValue);
+        }
+
+        scale(this.scaleValue);
+        strokeWeight(this.weigth);
+        textAlign(CENTER, CENTER);
+        text("o", 0, 0);
+        pop();
+    }
+}
