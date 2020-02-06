d28a3e2f0b4c7047c9be2361e9cd4f1feaf39b52
FernandoPinto <fernando.esparrinha@gmail.com>
2020-02-06 21:16:01 +0000

    Fix Mobile bug

--- wavetunnel.js
+++ wavetunnel.js
@@ -16,8 +16,8 @@ function setup() {
   if (isMobileDevice()) {
     createCanvas(windowWidth, windowHeight);
   } else {
-    createCanvas(1080, 1080);
-    // createCanvas(windowWidth, windowHeight);
+    // createCanvas(1080, 1080);
+    createCanvas(windowWidth, windowHeight);
     // The following command will transform every frame captured by CCapture into an mp4 video
     // ffmpeg -r 30 -f image2 -s 1080x1080 -i "%07d.jpg" -vcodec libx264 -crf 17 -pix_fmt yuv420p output.mp4
     capturer = new CCapture({ format: 'jpg', framerate: 30, name: 'Wavetunnel-' + Date.now(), verbose: false });
