23ed407680c1223e8a7d360b1cf93fdc1fd278f3
FernandoPinto <fernando.esparrinha@gmail.com>
2020-03-15 18:53:07 +0000

    Refactor variables

--- helpers/variables.js
+++ helpers/variables.js
@@ -3,13 +3,10 @@ let rectangleWave;
 let translateX = 0;
 let translateY = 0;
 
-let easing = 0.05;
-
 let fps;
 let capturer;
 
-let song, fft;
-let dx;
+let song, img, fft;
 
 let globalSettings;
 
