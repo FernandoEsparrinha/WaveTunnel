e85b5cdbac4ac11b3e74c07027f284032b55d7e3
FernandoPinto <fernando.esparrinha@gmail.com>
2020-03-15 18:47:57 +0000

    Add getIPAddresses() function in order to setup OSC server

--- helpers/functions.js
+++ helpers/functions.js
@@ -32,3 +32,22 @@ function ease(origin, destiny) {
   let dx = destiny - origin;
   return dx * easing;
 }
+
+function getIPAddresses() {
+  var os = require("os"),
+    interfaces = os.networkInterfaces(),
+    ipAddresses = [];
+
+  for (var deviceName in interfaces) {
+    var addresses = interfaces[deviceName];
+    for (var i = 0; i < addresses.length; i++) {
+      var addressInfo = addresses[i];
+      if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+        ipAddresses.push(addressInfo.address);
+      }
+    }
+  }
+
+  return ipAddresses;
+};
+
